b6885cde0e18c5921236b2e653f9d4c8b3d0fed8
hirotsubo <32426863+hirotsubo@users.noreply.github.com>
2024-08-09 16:58:09 +0900

    Update streamlit_app.py
    day10

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,14 +4,27 @@ import pandas as pd
 import streamlit as st
 from datetime import time, datetime 
 
-st.header('Line chart')
 
-chart_data = pd.DataFrame(
-    np.random.randn(20, 3),
-    columns = ['a','b','c']
+
+
+st.header('st.selectbox')
+
+option = st.selectbox(
+    'What is your favorite color?',
+    ('Blue', 'Red', 'Green')
 )
 
-st.line_chart(chart_data)
+st.write('Your favorite color is ', option)
+
+# day9
+# st.header('Line chart')
+
+# chart_data = pd.DataFrame(
+#     np.random.randn(20, 3),
+#     columns = ['a','b','c']
+# )
+
+# st.line_chart(chart_data)
 
 
 
