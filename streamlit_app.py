f4500a2f921e6e4ff1dd138c1028fd761dfdba74
hirotsubo <32426863+hirotsubo@users.noreply.github.com>
2024-08-09 15:23:35 +0900

    Update streamlit_app.py
    day8

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,29 +2,57 @@ import numpy as np
 import altair as alt
 import pandas as pd
 import streamlit as st
+from datetime import time, datetime 
 
 
-st.write('st.write')
+st.header('st.slider')
 
-st.write('hello, *world!* :sunglasses:')
+st.subheader('Slider')
 
-st.write(1234)
+age=st.slider('How old are you?', 0, 130, 25)
+st.write("I'm", age, 'years old')
 
-df = pd.DataFrame({
-    'first column': [1,2,3,4],
-    'second column':[10,20,30,40]
-})
 
-st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+st.subheader('Range time slider')
 
-df2 = pd.DataFrame(
-    np.random.randn(200, 3),
-    columns=['a','b','c'])
-
-c=alt.Chart(df2).mark_circle().encode(
-    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c']
+appointment=st.slider(
+    "Schedule your appointment:",
+    value=(time(11, 30), time(12,45))
 )
-st.write(c)
+st.write("You're scheduled for:", appointment)
+
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+    "When do you start?",
+    value=datetime(2020, 1, 1, 9, 30),
+    format="MM/DD/YY - hh:mm"
+)
+st.write("Start time:", start_time)
+
+
+# ~day7
+# st.write('st.write')
+
+# st.write('hello, *world!* :sunglasses:')
+
+# st.write(1234)
+
+# df = pd.DataFrame({
+#     'first column': [1,2,3,4],
+#     'second column':[10,20,30,40]
+# })
+
+# st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# df2 = pd.DataFrame(
+#     np.random.randn(200, 3),
+#     columns=['a','b','c'])
+
+# c=alt.Chart(df2).mark_circle().encode(
+#     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c']
+# )
+# st.write(c)
 
 
 # day5
